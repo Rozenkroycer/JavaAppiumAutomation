f4d48d9aabf3292e5c2b7bbc2255890280560be6
Rozenkroycer <pavelbulic@gmail.com>
2022-11-19 23:31:50 +0100

    Homework 1.3
    Homework 1.3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,7 +1,22 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainClassTest {
+
+    @Test
+    public void testGetClassString(){
+
+        MainClass MainClass = new MainClass();
+        String source_string = MainClass.getClassString();
+        String first_word = "hello";
+        String second_word = "Hello";
+        Assert.assertTrue("There are no words 'Hello' and 'hello' in the string", source_string.contains(first_word));
+        Assert.assertTrue("There are no words 'Hello' and 'hello' in the string", source_string.contains(second_word));
+
+    }
 
     @Test
     public void testGetClassNumber(){

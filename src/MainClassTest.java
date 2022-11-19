ba5082c74488271c99483c2582216314cf614e36
Rozenkroycer <pavelbulic@gmail.com>
2022-11-19 22:43:33 +0100

    Homework 1.2
    Homework 1.2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,6 +4,16 @@ import org.junit.Test;
 public class MainClassTest {
 
     @Test
+    public void testGetClassNumber(){
+        int expected_number = 45;
+
+        MainClass MainClass = new MainClass();
+        int actual_number = MainClass.getClassNumber();
+
+        Assert.assertTrue("Actual number lower or equal 45", actual_number > expected_number);
+    }
+
+    @Test
     public void testGetLocalNumber(){
         MainClass MainClass = new MainClass();
         int actual_number = MainClass.getLocalNumber();
